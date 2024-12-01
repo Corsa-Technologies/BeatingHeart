01cad576877c23230584d6add869cc93222797af
LucianoFO11 <lucianosaulo05@gmail.com>
2024-11-30 22:48:27 -0300

    adicionado void

---
+++ scritps/void.gd
@@ -0,0 +1,5 @@
+extends Area2D
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == 'Personagem' and CharacterBody2D:  # Verifica se o objeto que entrou é o personagem
+		body.global_position = Vector2(-914, 1180)  # Teleporta para o ponto (0, 0)
